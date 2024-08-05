73c3e920f497f19b300eab01b8320db19391d03d
A-myoen <24ammccoy@gmail.com>
2024-08-05 22:40:08 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,9 +22,32 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id]) 
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id]) 
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :image, :itemname, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :shippingcost_id, :price).merge(user_id: current_user.id)
   end
+
+  def redirectove_to_index
+    redirect_to root_path unless @item.user_id == current_user.id
+  end
+
 end
+
